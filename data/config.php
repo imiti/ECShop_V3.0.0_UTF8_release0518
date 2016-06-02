e13776d1c040796cc896937a3be30927233a4e02
nowphp <894583146@qq.com>
2016-06-02 11:24:45 +0800

    test
    test

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "172.168.6.6:3306";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "njw88";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','cbd6934ada9c10c0836badb9650e084f');
+
+?>
---
+++ temp/query_caches/sqlcache_config_file_43b9cde5d8ba6ccee8ad983c8fad735a.php
@@ -0,0 +1,6 @@
+<?php
+$this->mysql_config_cache_file_time = 1464775165;
+$this->timeline = 332;
+$this->timezone = 0;
+$this->platform = 'OTHER';
+?>
